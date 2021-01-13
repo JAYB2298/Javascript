c1306ae0db4f74a536da68d35740da724afce465
JAYB2298 <jaybaria221998@gmail.com>
2021-01-13 17:27:17 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,5 @@
+let n = 10;
+let v = 2
+const multiply = v-- * ++n;
+const add = --v + n++;
+console.log("multiply:" + multiply + "  add:" + add);
---
+++ script1.js
@@ -0,0 +1,33 @@
+const massmark = 78;
+const heightmark = 1.69;
+const massjohn = 92;
+const heightjohn = 1.95;
+
+const bmimark = massmark / heightmark ** 2;
+const bmijohn = massjohn / heightjohn ** 2;
+if (bmimark > bmijohn) {
+    console.log('marks ${ bmimark }');
+
+} else {
+    console.log("mark's  " + bmimark + " higher then " + bmijohn);
+}
+/*console outputa = 'ok, ${bmi}';
+console.log(outputa);
+*/
+let sarahage = 5
+if (sarahage > 18) {
+    console.log('sarah can drive');
+}
+else {
+    console.log("sarah can't drive");
+}
+
+const birthyear = 1998;
+let centuary;
+if (birthyear >= 2000) {
+    centuary = 21;
+} else {
+    centuary = 20;
+}
+console.log(centuary);
+
